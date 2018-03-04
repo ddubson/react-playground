89444727064db877058dd81cf6b15ebb12b8b383
Dmitriy Dubson <d.dubson@gmail.com>
2018-03-04 11:40:30 -0500

    Add sample of overridden props in test case

--- __tests__/components/ReduxUpdateExampleSpec.jsx
+++ __tests__/components/ReduxUpdateExampleSpec.jsx
@@ -3,17 +3,18 @@ import { shallow } from 'enzyme';
 import { ReduxUpdateExample } from '../../src/components/ReduxUpdateExample';
 
 describe('ReduxUpdateExample', () => {
-  const props = {
+  const props = overriddenProps => ({
     clickCount: 2,
     increaseClickCount: jest.fn(),
     decreaseClickCount: jest.fn(),
-  };
+    ...overriddenProps,
+  });
 
   it('should display Hello text', () => {
     const wrapper = shallow(<ReduxUpdateExample
-      {...props}
+      {...props({ clickCount: 12 })}
     />);
 
-    expect(wrapper.find('[className="card-text"]').text()).toEqual('Click Count: 2');
+    expect(wrapper.find('[className="card-text"]').text()).toEqual('Click Count: 12');
   });
 });
